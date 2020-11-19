9f8f362345e933ad034226baa44f44af58493cc4
supervueman <supervueman@gmail.com>
2020-11-19 17:48:12 +0400

    Add nest configuration

--- server/dist/main.js
+++ server/dist/main.js
@@ -2,9 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const serverPort = process.env.SERVER_PORT;
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    await app.listen(3000);
+    await app.listen(serverPort, () => {
+        console.info(`Server start on http://localhost:${serverPort}`);
+    });
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const serverPort = process.env.SERVER_PORT;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(serverPort, () => {
+    console.info(`Server start on http://localhost:${serverPort}`)
+  });
 }
 bootstrap();
